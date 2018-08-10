78fdb6849dcfa0b783733c2e145dc4eff3472c5a
Adrian DC <radian.dc@gmail.com>
2018-08-10 20:39:15 +0200

    display: Support AOSP build environment
    Change-Id: I05dab5f0f92522302bf1fa0fa6bf078fd52cfeab
    Signed-off-by: k-teja <tejachaitu96@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,21 +1,15 @@
-LOCAL_PATH := $(call my-dir)
-# TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
-ifneq ($(TARGET_BOARD_AUTO),true)
-
-ifneq ($(filter msm8x84,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8084)
-else ifneq ($(filter msm8x26,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8226)
-else ifneq ($(filter msm8992,$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,msm8994)
-else ifneq ($(filter msm8909,$(TARGET_BOARD_PLATFORM)),)
-  ifeq ($(TARGET_SUPPORTS_QCOM_3100),true)
-    include $(call all-named-subdir-makefiles,msm8909w_3100)
-  else
-    include $(call all-named-subdir-makefiles,msm8909)
-  endif
-else ifneq ($(wildcard $(LOCAL_PATH)/$(TARGET_BOARD_PLATFORM)),)
-  include $(call all-named-subdir-makefiles,$(TARGET_BOARD_PLATFORM))
+display-hals := libgralloc libgenlock libcopybit libvirtual
+display-hals += libhwcomposer liboverlay libqdutils libexternal libqservice
+display-hals += libmemtrack
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
 endif
 
+ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
+    include $(call all-named-subdir-makefiles,$(display-hals))
+else
+ifneq ($(filter msm8226 msm8x26 msm8960 msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
+    #This is for mako since it doesn't have the QCOM make functions
+    include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
